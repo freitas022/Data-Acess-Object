6607a2209029957746fc1e041abfb43c24934aa6
Matheus Freitas <84873099+freitas022@users.noreply.github.com>
2022-12-30 14:19:55 -0300

    findBy implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,17 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.DAO.DAOFactory;
+import model.DAO.SellerDAO;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, " e-commerce");
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		Seller seller = sellerDAO.findById(3);
+		
 		System.out.println(seller);
 	}
 }
---
+++ src/model/DAO/DAOFactory.java
@@ -0,0 +1,11 @@
+package model.DAO;
+
+import db.DB;
+import model.DAO.impl.SellerDaoJDBC;
+
+public class DAOFactory {
+	
+	public static SellerDAO createSellerDAO() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+}
