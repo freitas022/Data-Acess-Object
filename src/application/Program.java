1775fec330cd89f32a232d383e76740b9acf5bc2
Matheus Freitas <84873099+freitas022@users.noreply.github.com>
2022-12-31 10:10:36 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,9 +17,15 @@ public class Program {
 		Seller seller = sellerDAO.findById(3);		
 		System.out.println(seller);
 		// -------------------------------------------------------------------
-		System.out.println("=== TESTE 02: SELLER FINDBY DEPARTMENT ===");
+		System.out.println("\n=== TESTE 02: SELLER FINDBY DEPARTMENT ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDAO.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		//-------------------------------------------------------------------
+		System.out.println("\n=== TESTE 03: SELLER FINDALL() ===");
+		list = sellerDAO.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
